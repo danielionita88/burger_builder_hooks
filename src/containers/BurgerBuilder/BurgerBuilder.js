ac9cf68b34a44d81c41177ce7ffed4b8d22dcfbb
Daniel Ionita <danielionita88@yahoo.com>
2020-04-01 19:22:11 -0500

    animate order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends React.Component{
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
     }
 
     updatePurchaseState= (ingredients) =>{
@@ -74,6 +75,10 @@ class BurgerBuilder extends React.Component{
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler= () => {
+        this.setState({ purchasing: true })
+    }
+
     render(){
         const disabledInfo={
             ...this.state.ingredients
@@ -85,7 +90,7 @@ class BurgerBuilder extends React.Component{
 
         return (
             <Aux>
-                <Modal>
+                <Modal show={this.state.purchasing}>
                     <OrderSummary ingredients={this.state.ingredients}/> 
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
@@ -95,6 +100,7 @@ class BurgerBuilder extends React.Component{
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
                     purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}
                 />
             </Aux>
         );
